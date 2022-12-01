894c258e12dc2682d6aaf6346c538b63d5145481
Gary Justin <7003114+gejustin@users.noreply.github.com>
2022-12-01 10:18:31 -0500

    add aliased dependency

--- packages/ui-components/src/lib/ui-components.tsx
+++ packages/ui-components/src/lib/ui-components.tsx
@@ -1,12 +1,12 @@
+import { isNotNull } from '@myorg/common';
+
+const test = isNotNull<string>('hello');
+
 /* eslint-disable-next-line */
 export interface UiComponentsProps {}
 
 export function UiComponents(props: UiComponentsProps) {
-  return (
-    <div>
-      <h1>Welcome to UiComponents!</h1>
-    </div>
-  );
+  return <div>{test && <h1>Welcome to UiComponents!</h1>}</div>;
 }
 
 export default UiComponents;
